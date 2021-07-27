ccfed576dc35eb4e0cf2c0cbc4d0c25c1a59f59a
GeraldoBarros <geraldo.barros@neurobots.com.br>
2021-07-27 17:56:47 -0300

    funcionalidade de ssr fazendo a chamada a api pelo servidor node do next e retornando o preco do produto via props.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,20 @@
 /* eslint-disable @next/next/no-img-element */
+import { GetServerSideProps } from "next";
 import Head from "next/head";
+
+import { stripe } from "../services/stripe";
+
 import { SubscribeButton } from "../components/SubscribeButton";
 import styles from "./home.module.scss";
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -18,13 +29,34 @@ export default function Home() {
           </h1>
           <p>
             Get access to all publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
-        <img src="/images/avatar.svg" alt="Girld coding" />
+        <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  // retrive retorna apenas do produto em questão
+  const price = await stripe.prices.retrieve("price_1JFzgBCZe9BA06FHic2Q95G7", {
+    expand: ["product"],
+  });
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product,
+    },
+  };
+};
