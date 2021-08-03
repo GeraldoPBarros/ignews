39633e9be3208ccbf5a422253ddad2f19cda4496
GeraldoBarros <geraldo.barros@neurobots.com.br>
2021-08-03 08:17:27 -0300

    correcao do diretorio para atenticacao e autenticacao com o signIn. tambem foi feito o signOut e Provider passando session para todos os componentes.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,21 @@
 import { AppProps } from "next/app";
 import { Header } from "../components/Header";
+import { Provider as NextAuthProvider } from "next-auth/client";
 
 import "../../styles/global.scss";
 
+/**
+ *
+ * @param pageProps
+ * @returns informações da sessão ativa sempre que a página é atualizada
+ */
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <NextAuthProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </NextAuthProvider>
   );
 }
 
