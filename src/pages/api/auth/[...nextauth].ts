1c09e3267fee92527630e8334db1edc551e63af7
GeraldoBarros <geraldo.barros@neurobots.com.br>
2021-08-11 08:52:26 -0300

    configuracao de conexao com o fauna, callback signIn do NextAuth para salvar no fauna informacao de login do usuario.

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from "faunadb";
+
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
+
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -10,6 +14,15 @@ export default NextAuth({
       scope: "read:user",
     }),
   ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user;
+
+      await fauna.query(q.Create(q.Collection("users"), { data: { email } }));
+
+      return true;
+    },
+  },
 });
 
 // escops: definem quais informações poderão ser acessadas dos usuários
