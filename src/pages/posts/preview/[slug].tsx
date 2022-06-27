a316bb097c85f285518a6d1545fbc2a2c029cf78
GeraldoBarros <geraldo.barros@neurobots.com.br>
2022-06-27 07:24:02 -0300

    ajustes.

---
+++ src/pages/posts/preview/[slug].tsx
@@ -0,0 +1,105 @@
+import { GetStaticPaths, GetStaticProps } from "next";
+import { useSession } from "next-auth/client";
+import Head from "next/head";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { RichText } from "prismic-dom";
+import { useEffect } from "react";
+import { getPrismicClient } from "../../../services/prismic";
+
+import styles from "../post.module.scss";
+
+interface PostPreviewProps {
+  post: {
+    slug: string;
+    title: string;
+    content: string;
+    updatedAt: string;
+  };
+}
+
+/* 
+  dangerouslySetInnerHTML vai adicionar o conteúdo 
+  vindo na variávei para formato HTML
+*/
+
+export default function PostPreview({ post }: PostPreviewProps) {
+  const [session] = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    console.log("Session: ", session);
+    if (session?.activeSubscription) {
+      router.push(`/posts/${post.slug}`);
+    }
+  }, [session]);
+
+  return (
+    <>
+      <Head>
+        <title>{post.title} | Ignews</title>
+      </Head>
+
+      <main className={styles.container}>
+        <article className={styles.post}>
+          <h1>{post.title}</h1>
+          <time>{post.updatedAt}</time>
+          <div
+            className={`${styles.postContent} ${styles.previewContent}`}
+            dangerouslySetInnerHTML={{ __html: post.content }}
+          />
+          <div className={styles.continueReading}>
+            Wanna continue reading?
+            <Link href="/">
+              <a>Subscribe now 🤗</a>
+            </Link>
+          </div>
+        </article>
+      </main>
+    </>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [
+      // vazio igual a todos os posts
+      /* {
+        params: {
+          slug: "next.js---novidades-na-versao-10-e-atualizacao-do-blog",
+        },
+      }, */
+    ],
+    fallback: "blocking", // true, false, blocking
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { slug } = params;
+
+  const prismic = getPrismicClient();
+
+  const response = await prismic.getByUID("post", String(slug), {});
+
+  // console.log(JSON.stringify(response, null, 2));
+
+  const post = {
+    slug,
+    title: RichText.asText(response.data.title),
+    content: RichText.asHtml(response.data.content.splice(0, 3)),
+    updatedAt: new Date(response.last_publication_date).toLocaleDateString(
+      "pt-BR",
+      {
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+      }
+    ),
+  };
+  return {
+    props: {
+      post,
+    },
+    redirect: 60 * 30, // 30 minutos
+  };
+};
--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -1,6 +1,8 @@
 import Prismic from "@prismicio/client";
 
 export function getPrismicClient(req?: unknown) {
+  console.log("endpoint: ", process.env.PRISMIC_ENDPOINT);
+
   const prismic = Prismic.client(process.env.PRISMIC_ENDPOINT, {
     req,
     accessToken: process.env.PRISMIC_ACCESS_TOKEN,
