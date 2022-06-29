70b1c7cd7e916e70cc673a70de2ba881342d4077
GeraldoBarros <geraldo.barros@neurobots.com.br>
2022-06-29 08:31:57 -0300

    tests para signInButton e subscribeButton.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import Link from "next/link";
 import { ActiveLink } from "../ActiveLink";
-import { SignInButton } from "../SignInButton/indes";
+import { SignInButton } from "../SignInButton";
 import styles from "./styles.module.scss";
 
 export function Header() {
